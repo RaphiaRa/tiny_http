e0aae4a902ce68e64880fabaa4285d55589a756e
Raphael Schlarb <info@raphael.schlarb.one>
2024-09-26 16:10:42 +0200

    Remove unused function declarations

--- src/th_io_op.h
+++ src/th_io_op.h
@@ -8,15 +8,6 @@
 #include <stddef.h>
 
 TH_PRIVATE(th_err)
-th_io_op_openat(void* self, size_t* result) TH_MAYBE_UNUSED;
-
-TH_PRIVATE(th_err)
-th_io_op_open(void* self, size_t* result) TH_MAYBE_UNUSED;
-
-TH_PRIVATE(th_err)
-th_io_op_close(void* self, size_t* result) TH_MAYBE_UNUSED;
-
-TH_PRIVATE(th_err)
 th_io_op_read(void* self, size_t* result) TH_MAYBE_UNUSED;
 
 TH_PRIVATE(th_err)

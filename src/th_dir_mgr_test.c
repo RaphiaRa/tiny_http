7b780873a2383711815c6c283ce289914c07e2f5
Raphael Schlarb <info@raphael.schlarb.one>
2024-09-28 09:53:23 +0200

    Fix Crash When Adding Non-Existent Directory and Add Respective Tests
    - Fix crash that occurred when a non-existent directory was added.
    - Add tests to verify correct behavior when adding non-existent directories.

--- src/th_dir_mgr_test.c
+++ src/th_dir_mgr_test.c
@@ -1,5 +1,12 @@
 #include "th_dir_mgr.h"
+#include "th_mock_syscall.h"
 #include "th_test.h"
+
+static int
+bad_open(void)
+{
+    return -TH_ENOENT;
+}
 
 TH_TEST_BEGIN(dir_mgr)
 {
@@ -19,5 +26,15 @@ TH_TEST_BEGIN(dir_mgr)
         th_dir_mgr_deinit(&mgr);
     }
     TH_TEST_CASE_END
+    TH_TEST_CASE_BEGIN(dir_mgr_add_bad_open)
+    {
+        th_mock_syscall_get()->open = bad_open;
+        th_dir_mgr mgr = {0};
+        th_dir_mgr_init(&mgr, NULL);
+        TH_EXPECT(th_dir_mgr_add(&mgr, TH_STRING("test"), TH_STRING("/")) == TH_ERR_SYSTEM(TH_ENOENT));
+        TH_EXPECT(th_dir_mgr_get(&mgr, TH_STRING("test")) == NULL);
+        th_dir_mgr_deinit(&mgr);
+    }
+    TH_TEST_END
 }
 TH_TEST_END

862def9662d9b53f2f1f1d3af361a468c557bf82
Raphael Schlarb <info@raphael.schlarb.one>
2024-10-06 14:33:26 +0200

    fix: initialize variable on init and add test to catch previous bug

--- src/th_request_parser_test.c
+++ src/th_request_parser_test.c
@@ -42,5 +42,22 @@ TH_TEST_BEGIN(request_parser)
         th_request_deinit(&request);
     }
     TH_TEST_CASE_END
+    TH_TEST_CASE_BEGIN(parse_path_and_body)
+    {
+        th_request request;
+        th_request_init(&request, NULL);
+        th_request_parser parser;
+        th_request_parser_init(&parser);
+        th_string data = TH_STRING("POST /test HTTP/1.1\r\nHost: example.com\r\nContent-Length: 11\r\n\r\nHello World");
+        size_t parsed = 0;
+        TH_EXPECT(th_request_parser_parse(&parser, &request, data, &parsed) == TH_ERR_OK);
+        TH_EXPECT(parsed == data.len);
+        TH_EXPECT(request.method == TH_METHOD_POST);
+        TH_EXPECT(th_heap_string_eq(&request.uri_path, TH_STRING("/test")));
+        TH_EXPECT(request.version == 1);
+        TH_EXPECT(TH_STRING_EQ(request.body, "Hello World"));
+        th_request_deinit(&request);
+    }
+    TH_TEST_CASE_END
 }
 TH_TEST_END

66199b898948c7685cd0aa10741b1d1aa96318b4
Raphael Schlarb <info@raphael.schlarb.one>
2024-10-12 12:23:38 +0200

    refactor: clean up `th_response`
    - Remove obsolete allocation optimizations.

--- src/th_response.h
+++ src/th_response.h
@@ -13,37 +13,20 @@
 #include "th_socket.h"
 /* th_response begin */
 
-#define TH_RESPONSE_MAX_IOV 32
-
-/** th_last_chunk_type
- *  TH_CHUNK_TYPE_HEADER: The last chunk written was a header chunk.
- *  TH_CHUNK_TYPE_BODY: The last chunk written was a body chunk.
- *  The user can set headers after writing the body,
- *  we need to detect this case so that we can setup the
- *  buffers for the headers correctly.
- */
-typedef enum th_last_chunk_type {
-    TH_CHUNK_TYPE_NONE,
-    TH_CHUNK_TYPE_HEADER,
-    TH_CHUNK_TYPE_BODY,
-} th_last_chunk_type;
+// 3 = start line + headers + body
+#define TH_RESPONSE_MAX_CHUNK_NUM 3
 
 struct th_response {
-    th_context* context;
-    th_allocator* allocator;
-    th_iov iov[TH_RESPONSE_MAX_IOV];
-    th_iov* header_buf;
-    size_t cur_header_buf_len;
-    size_t cur_header_buf_pos;
-
-    char header_is_set[TH_HEADER_ID_MAX];
+    th_heap_string headers;
     th_heap_string body;
-    int is_file;
+    th_iov iov[TH_RESPONSE_MAX_CHUNK_NUM];
+    th_allocator* allocator;
     th_fcache* fcache;
     th_fcache_entry* fcache_entry;
     size_t file_len;
-    th_last_chunk_type last_chunk_type;
     th_code code;
+    bool header_is_set[TH_HEADER_ID_MAX];
+    bool is_file;
     // Set this to true if we have a HEAD request, so that we only write headers.
     bool only_headers;
 };

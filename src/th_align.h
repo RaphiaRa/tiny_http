4a10627801438af735f653006f7642ef1ae46cd2
Raphael Schlarb <info@raphael.schlarb.one>
2025-09-03 00:55:22 +0200

    fix: avoid non-standard syntax
    Defining types withing offsetof is not allwed in pre C23.
    Use null pointer trick to get the offset.

--- src/th_align.h
+++ src/th_align.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define TH_ALIGNOF(type) offsetof(struct { char c; type member; }, member)
+#define TH_ALIGNOF(type) ((size_t)&(((struct { char c; type member; }*)0)->member))
 #define TH_ALIGNAS(align, ptr) ((void*)(((uintptr_t)(ptr) + ((align) - 1)) & ~((align) - 1)))
 #define TH_ALIGNUP(n, align) (((n) + (size_t)(align) - 1) & ~((size_t)(align) - 1))
 #define TH_ALIGNDOWN(n, align) ((n) & ~((align) - 1))

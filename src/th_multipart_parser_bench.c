241e9345eac551dd3e04f4be56dda04ddbccf850
Raphael Schlarb <info@raphael.schlarb.one>
2026-08-04 04:30:52 -0500

    refactor: keep bench fixture setup next to its case, not hoisted to file scope
    - split TH_BENCH_CASE_BEGIN/END (runs once, untimed - fixture setup
      lives here) from a new TH_BENCH_RUN_BEGIN/END (the timed loop)
    - register the new macros as clang-format StatementMacros

--- src/th_multipart_parser_bench.c
+++ src/th_multipart_parser_bench.c
@@ -20,94 +20,113 @@ th_multipart_bench_build_large_body(char* buf, size_t buf_size, size_t content_l
     return pos;
 }
 
-static const th_str th_bench_content_type = TH_STR("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
-
-static const th_str th_bench_single_field_body = TH_STR("--boundary\r\n"
-                                                        "Content-Disposition: form-data; name=\"field1\"\r\n\r\n"
-                                                        "value1\r\n"
-                                                        "--boundary--\r\n");
-
-static const th_str th_bench_ten_fields_body = TH_STR("--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"a\"\r\n\r\n1\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"b\"\r\n\r\n2\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"c\"\r\n\r\n3\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"d\"\r\n\r\n4\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"e\"\r\n\r\n5\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"f\"\r\n\r\n6\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"g\"\r\n\r\n7\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"h\"\r\n\r\n8\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"i\"\r\n\r\n9\r\n"
-                                                      "--boundary\r\n"
-                                                      "Content-Disposition: form-data; name=\"j\"\r\n\r\n10\r\n"
-                                                      "--boundary--\r\n");
-
-static char th_bench_content_length_buf[4096 + 256];
-static char th_bench_boundary_scan_buf[4096 + 256];
-
 TH_BENCH_BEGIN(multipart_parser)
 {
-    size_t content_length_body_len = th_multipart_bench_build_large_body(th_bench_content_length_buf, sizeof(th_bench_content_length_buf), 4096, true);
-    th_str content_length_body = th_str_make(th_bench_content_length_buf, content_length_body_len);
-
-    size_t boundary_scan_body_len = th_multipart_bench_build_large_body(th_bench_boundary_scan_buf, sizeof(th_bench_boundary_scan_buf), 4096, false);
-    th_str boundary_scan_body = th_str_make(th_bench_boundary_scan_buf, boundary_scan_body_len);
-
     TH_BENCH_CASE_BEGIN(parse_boundary_from_content_type, 100000)
     {
-        th_str boundary;
-        th_multipart_parser_boundary(th_bench_content_type, &boundary);
+        th_str content_type = TH_STR("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_str boundary;
+            th_multipart_parser_boundary(content_type, &boundary);
+        }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(parse_single_small_field, 100000)
     {
-        th_multipart_parser parser;
-        th_multipart_parser_init(&parser, th_bench_single_field_body, TH_STR("boundary"));
-        while (!th_multipart_parser_done(&parser)) {
-            th_multipart_part part;
-            th_multipart_parser_next(&parser, &part);
+        th_str body = TH_STR("--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"field1\"\r\n\r\n"
+                             "value1\r\n"
+                             "--boundary--\r\n");
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_multipart_parser parser;
+            th_multipart_parser_init(&parser, body, TH_STR("boundary"));
+            while (!th_multipart_parser_done(&parser)) {
+                th_multipart_part part;
+                th_multipart_parser_next(&parser, &part);
+            }
         }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(parse_ten_fields, 10000)
     {
-        th_multipart_parser parser;
-        th_multipart_parser_init(&parser, th_bench_ten_fields_body, TH_STR("boundary"));
-        while (!th_multipart_parser_done(&parser)) {
-            th_multipart_part part;
-            th_multipart_parser_next(&parser, &part);
+        th_str body = TH_STR("--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"a\"\r\n\r\n1\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"b\"\r\n\r\n2\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"c\"\r\n\r\n3\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"d\"\r\n\r\n4\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"e\"\r\n\r\n5\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"f\"\r\n\r\n6\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"g\"\r\n\r\n7\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"h\"\r\n\r\n8\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"i\"\r\n\r\n9\r\n"
+                             "--boundary\r\n"
+                             "Content-Disposition: form-data; name=\"j\"\r\n\r\n10\r\n"
+                             "--boundary--\r\n");
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_multipart_parser parser;
+            th_multipart_parser_init(&parser, body, TH_STR("boundary"));
+            while (!th_multipart_parser_done(&parser)) {
+                th_multipart_part part;
+                th_multipart_parser_next(&parser, &part);
+            }
         }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(parse_4kb_file_via_content_length, 10000)
     {
-        th_multipart_parser parser;
-        th_multipart_parser_init(&parser, content_length_body, TH_STR("boundary"));
-        while (!th_multipart_parser_done(&parser)) {
-            th_multipart_part part;
-            th_multipart_parser_next(&parser, &part);
+        char buf[4096 + 256];
+        size_t len = th_multipart_bench_build_large_body(buf, sizeof(buf), 4096, true);
+        th_str body = th_str_make(buf, len);
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_multipart_parser parser;
+            th_multipart_parser_init(&parser, body, TH_STR("boundary"));
+            while (!th_multipart_parser_done(&parser)) {
+                th_multipart_part part;
+                th_multipart_parser_next(&parser, &part);
+            }
         }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(parse_4kb_file_via_boundary_scan, 10000)
     {
-        th_multipart_parser parser;
-        th_multipart_parser_init(&parser, boundary_scan_body, TH_STR("boundary"));
-        while (!th_multipart_parser_done(&parser)) {
-            th_multipart_part part;
-            th_multipart_parser_next(&parser, &part);
+        char buf[4096 + 256];
+        size_t len = th_multipart_bench_build_large_body(buf, sizeof(buf), 4096, false);
+        th_str body = th_str_make(buf, len);
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_multipart_parser parser;
+            th_multipart_parser_init(&parser, body, TH_STR("boundary"));
+            while (!th_multipart_parser_done(&parser)) {
+                th_multipart_part part;
+                th_multipart_parser_next(&parser, &part);
+            }
         }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 }
--- src/th_str_bench.c
+++ src/th_str_bench.c
@@ -1,41 +1,60 @@
 #include "th_bench.h"
 #include "th_str.h"
 
-static char th_bench_needle_near_end_buf[4096];
-static char th_bench_no_match_buf[4096];
-
 TH_BENCH_BEGIN(str)
 {
-    for (size_t i = 0; i < sizeof(th_bench_needle_near_end_buf); i++)
-        th_bench_needle_near_end_buf[i] = 'a' + (char)(i % 26);
-    th_bench_needle_near_end_buf[sizeof(th_bench_needle_near_end_buf) - 1] = ';';
-    th_str needle_near_end_str = th_str_make(th_bench_needle_near_end_buf, sizeof(th_bench_needle_near_end_buf));
-
-    for (size_t i = 0; i < sizeof(th_bench_no_match_buf); i++)
-        th_bench_no_match_buf[i] = 'a' + (char)(i % 26);
-    th_str no_match_str = th_str_make(th_bench_no_match_buf, sizeof(th_bench_no_match_buf));
-
     TH_BENCH_CASE_BEGIN(find_first_short_string, 100000)
     {
-        th_str_find_first(TH_STR("Content-Type: text/plain"), 0, ':');
+        th_str str = TH_STR("Content-Type: text/plain");
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_str_find_first(str, 0, ':');
+        }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(find_first_4kb_match_near_end, 10000)
     {
-        th_str_find_first(needle_near_end_str, 0, ';');
+        static char buf[4096];
+        for (size_t i = 0; i < sizeof(buf); i++)
+            buf[i] = 'a' + (char)(i % 26);
+        buf[sizeof(buf) - 1] = ';';
+        th_str str = th_str_make(buf, sizeof(buf));
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_str_find_first(str, 0, ';');
+        }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(find_first_4kb_no_match, 10000)
     {
-        th_str_find_first(no_match_str, 0, ';');
+        static char buf[4096];
+        for (size_t i = 0; i < sizeof(buf); i++)
+            buf[i] = 'a' + (char)(i % 26);
+        th_str str = th_str_make(buf, sizeof(buf));
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_str_find_first(str, 0, ';');
+        }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 
     TH_BENCH_CASE_BEGIN(find_first_of_short_string, 1000000)
     {
-        th_str_find_first_of(TH_STR("name=\"a\""), 0, "=; ");
+        th_str str = TH_STR("name=\"a\"");
+
+        TH_BENCH_RUN_BEGIN
+        {
+            th_str_find_first_of(str, 0, "=; ");
+        }
+        TH_BENCH_RUN_END
     }
     TH_BENCH_CASE_END
 }
